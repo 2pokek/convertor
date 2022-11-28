1e329accdba8888c8873eb4831757df9327c4bb0
2pokek <alkuzmich6479@gmail.com>
2022-11-28 23:45:06 +0300

    4

--- convector/exchange/views.py
+++ convector/exchange/views.py
@@ -16,13 +16,16 @@ def exchange(request):
     if request.method == "POST":
         from_amount = float(request.POST.get('from-amount'))
         from_curr = request.POST.get('from-curr')
-        to_curr = request.POST.get('to_curr')
+        to_curr = request.POST.get('to-curr')
 
         converted_amount = round((currencies[to_curr] / currencies[from_curr]) * float(from_amount), 2)
 
         context = {
+            'from_curr': from_curr,
+            'to_curr': to_curr,
+            'from_amount': from_amount,
             'currencies': currencies,
-            'converted_amount': converted_amount,
+            'converted_amount': converted_amount
         }
 
         return render(request=request, template_name='exchange/index.html', context=context)
