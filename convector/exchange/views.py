b18d049010eeec589ca1561600ee0f16ee026416
2pokek <alkuzmich6479@gmail.com>
2022-11-28 23:36:45 +0300

    3

--- convector/exchange/views.py
+++ convector/exchange/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
+import requests
+
 
 def exchange(request):
-    name= 'mr'
+    response = requests.get(url='https://api.exchangerate-api.com/v4/latest/USD').json()
+    currencies = response.get('rates')
 
-    context = {
-        'name' : name
-    }
+    if request.method == "GET":
+        context = {
+            'currencies': currencies,
+        }
 
-    return render(request=request,template_name='exchange/index.html',context=context)
+        return render(request=request, template_name='exchange/index.html', context=context)
+
+    if request.method == "POST":
+        from_amount = float(request.POST.get('from-amount'))
+        from_curr = request.POST.get('from-curr')
+        to_curr = request.POST.get('to_curr')
+
+        converted_amount = round((currencies[to_curr] / currencies[from_curr]) * float(from_amount), 2)
+
+        context = {
+            'currencies': currencies,
+            'converted_amount': converted_amount,
+        }
+
+        return render(request=request, template_name='exchange/index.html', context=context)
